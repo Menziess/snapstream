5c2a1d778b086330226880faad87e8227f9e5e34
Menziess <stefan_schenk@hotmail.com>
2023-04-07 23:10:08 +0200

    Added tests for snap and stream.

--- tests/test__init__.py
+++ tests/test__init__.py
@@ -1,11 +1,22 @@
-from snapstream import snap, stream
+from snapstream import Conf, snap, stream
 
 
 def test_snap():
-    """Should ..."""
-    assert snap
+    """Should register iterable."""
+    Conf().iterables = set()
+
+    @snap(range(1))
+    def _(msg):
+        return msg
+
+    _, it = list(Conf().iterables).pop()
+    assert it == range(1)
 
 
 def test_stream():
-    """Should ..."""
+    """Should start processing an unsnapped iterable."""
+    Conf().iterables = set()
+    it = range(1)
+    iterable_key = str(id(it))
+    Conf().register_iterables((iterable_key, it))
     assert stream() is None
