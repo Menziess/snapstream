0986ad27557696f510a6c4ea899b94470a409114
Stefan Schenk <stefan_schenk@hotmail.com>
2023-05-02 22:22:18 +0200

    Got rid of autoapi.

--- docs/conf.py
+++ docs/conf.py
@@ -13,14 +13,10 @@ author = 'Menziess'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.viewcode', 'sphinx.ext.autodoc', 'autoapi.extension']
+extensions = ['sphinx.ext.viewcode', 'sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.venv/*']
-
-autoapi_type = 'python'
-autoapi_dirs = ['../snapstream']
-autoapi_ignore = ['*/__main__.py']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
